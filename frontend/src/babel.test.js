59889046222658767f59db1eab813ee0507def2c
Randy <randyharrogates@gmail.com>
2024-12-25 12:08:10 +0800

    chore: Moved test files

--- frontend/src/babel.test.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import React from "react";
-import { render } from "@testing-library/react";
-
-test("basic render test", () => {
-	const { getByText } = render(<div>Hello World</div>);
-	expect(getByText("Hello World")).toBeInTheDocument();
-});
--- frontend/src/react-router-dom.test.js
+++
@@ -1,5 +0,0 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-test("checks if router is resolved", () => {
-  expect(Router).toBeDefined();
-});
